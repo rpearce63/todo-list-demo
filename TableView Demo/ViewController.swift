b93b4701dbee8e706cc495e22a43d2ef47910537
Rick Pearce <rick.pearce.crzs@statefarm.com>
2018-04-26 17:13:11 -0500

    add checkmark

--- TableView Demo/ViewController.swift
+++ TableView Demo/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var enterText: UITextField!
+    
+    var todoList = [[String : Bool]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        if let storedData = UserDefaults.standard.array(forKey: "todoList") as? [[String : Bool]] {
+            todoList = storedData
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+   
+    
+    @IBAction func addBtnPressed(_ sender: Any) {
+        if enterText.text != "" {
+            todoList.append([enterText.text! : false])
+        }
+        tableView.reloadData()
+        enterText.text = ""
+        UserDefaults.standard.set(todoList, forKey: "todoList")
     }
-
+    
+//    func getData() -> [String] {
+//        return todoList
+//    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = (todoList[indexPath.row].keys.first)
+        cell?.accessoryType = (todoList[indexPath.row].values.first!) ? .checkmark : .none
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.accessoryType = cell?.accessoryType == .checkmark ? .none : .checkmark
+        todoList[indexPath.row].updateValue(cell?.accessoryType == .checkmark, forKey: todoList[indexPath.row].keys.first!)
+        tableView.deselectRow(at: indexPath, animated: true)
+        UserDefaults.standard.set(todoList, forKey: "todoList")
+    }
+    
+    
 
 }
+
 
